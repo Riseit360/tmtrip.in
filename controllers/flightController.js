79e5ea55ef42982d207e31f3e974380933cfbc8d
Taxmanager-Bittu <taxmanager.bittu@gmail.com>
2025-12-31 11:05:35 +0530

    update controller js

---
+++ controllers/flightController.js
@@ -0,0 +1,17 @@
+
+// Express Rout module import 
+var express = require('express');
+const axios = require("axios");
+
+
+
+
+// File and Functions
+class flightAPIDta {
+
+}
+
+
+
+// Export the flight all Funtion  class
+module.exports = flightAPIDta;
