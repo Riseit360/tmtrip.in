c436d32d9ff4173bd818f9310e94dbd8de9255cc
Taxmanager-Bittu <taxmanager.bittu@gmail.com>
2025-12-31 12:03:28 +0530

    update code for new

--- routes/FlightRoutes.js
+++ routes/FlightRoutes.js
@@ -1,14 +1,26 @@
 // routes/mainroutes.js
 const express = require("express");
 const router = express.Router();
- 
 
+// File and Functions import
+const flightAPIDta = require("../controllers/flightController");
+
+
+// File canvert in object
+const flightData = new flightAPIDta();
 
 
 
 // Flight Routes
 router.get('/flight-search', (req, res) => {
+    try {
+        // Pages Direcdtory
+        return res.status(200).render("flight/flight-search.ejs", {
+            title: "Dashboard"
+        });
+    } catch (error) {
 
+    }
 })
 
 
