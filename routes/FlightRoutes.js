7dba507a7abcbf565432828ead2a2e7b9ff780f7
Taxmanager-Bittu <taxmanager.bittu@gmail.com>
2025-12-31 15:19:42 +0530

    update for rout

--- routes/FlightRoutes.js
+++ routes/FlightRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 // File and Functions import
-const flightAPIDta = require("../controllers/flightController");
+const flightAPIDta = require("../controllers/flightController.js");
 
 
 // File canvert in object
@@ -12,7 +12,23 @@ const flightData = new flightAPIDta();
 
 
 // Flight Routes
-router.get('/flight-search', flightData.flightsearch, (req, res) => {
+router.get('/flight-search/list',  async(req, res)=>{
+    try {
+        const flightSearchResult = await flightData.flightsearch(req, res);
+        console.log('flightSearchResult: ', flightSearchResult);
+
+
+
+
+    } catch (error) {
+        
+    }
+})
+
+
+
+// Flight Pages
+router.get('/flight-search',  async (req, res) => {
     try {
         // Pages Direcdtory
         return res.status(200).render("flight/flight-search.ejs", {
